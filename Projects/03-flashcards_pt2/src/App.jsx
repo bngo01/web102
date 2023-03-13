60bfb5fba126ea7ae139df63bd3ab4085525e08f
Bao Ngo <b.ngo3048@gmail.com>
2023-03-12 22:07:14 -0400

    replicated proj2 in proj3 dir

---
+++ Projects/03-flashcards_pt2/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Template from "./components/Template.jsx"
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <Template />
+    </div>
+
+  )
+}
+
+export default App
