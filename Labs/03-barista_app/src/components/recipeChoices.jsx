a7f30281b4a3caf0d3fb594c876c1081ef9395de
Bao Ngo <b.ngo3048@gmail.com>
2023-03-12 21:36:04 -0400

    finished lab 3

---
+++ Labs/03-barista_app/src/components/recipeChoices.jsx
@@ -0,0 +1,26 @@
+import React, { Components, useEffect, useState } from "react"
+
+const RecipeChoices = ({handleChange, label, choices, currentVal}) => {
+    return (
+        <div>
+            <div className="radio-buttons">
+                <input
+                    type="text"
+                    name={label}
+                    value={currentVal}
+                    placeholder="Guess the ingredient..."
+                    onChange={handleChange}
+                    className = "textbox"
+                    />
+                {choices && choices.map((choice) => (
+                    <li key={choice}>
+                        <input id={choice} value={choice} name={label} type="radio" onChange={handleChange} checked={currentVal == choice}/>
+                        {choice}
+                    </li>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default RecipeChoices
