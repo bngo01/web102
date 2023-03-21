b8c1c1de2e9b3e469d2b8278787e991646b8879b
Bao Ngo <b.ngo3048@gmail.com>
2023-03-21 00:53:33 -0400

    fixed spacing

--- Labs/04-cap/src/components/gallery.jsx
+++ Labs/04-cap/src/components/gallery.jsx
@@ -1,29 +1,29 @@
 const Gallery = ({ images }) => {
 
-    return (
-      <div>
-        <h2> Your Screenshot Gallery!</h2>
-        <div className="image-container">
-            {images && images.length > 0 ? (
-                images.map((pic, index) => (
-                    <li className="gallery" key={index}>
-                      <img
-                        className="gallery-screenshot"
-                        src={pic}
-                        alt="Undefined screenshot from query"
-                        width="500"
-                      />
-                    </li>
-                  )
+  return (
+    <div>
+      <h2> Your Screenshot Gallery!</h2>
+      <div className="image-container">
+          {images && images.length > 0 ? (
+              images.map((pic, index) => (
+                  <li className="gallery" key={index}>
+                    <img
+                      className="gallery-screenshot"
+                      src={pic}
+                      alt="Undefined screenshot from query"
+                      width="500"
+                    />
+                  </li>
                 )
-            ) : (
-                <div>
-                <h3>You haven't made a screenshot yet!</h3>
-                </div>
-            )}
-        </div>
+              )
+          ) : (
+              <div>
+              <h3>You haven't made a screenshot yet!</h3>
+              </div>
+          )}
       </div>
-    );
-  };
-  
-  export default Gallery;
+    </div>
+  );
+};
+
+export default Gallery;
